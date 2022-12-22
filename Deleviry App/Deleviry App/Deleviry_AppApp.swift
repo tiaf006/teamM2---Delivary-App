b86200ac0a4348709c7885f6eb3f435647c07aee
tiaf006 <85888496+tiaf006@users.noreply.github.com>
2022-12-22 04:45:40 -0800

    Add files via upload

---
+++ Deleviry App/Deleviry App/Deleviry_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  Deleviry_AppApp.swift
+//  Deleviry App
+//
+//  Created by TAIF Al-zahrani on 28/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Deleviry_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
