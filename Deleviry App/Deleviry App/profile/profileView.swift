9d70ecca485e8eff289eb3c0463d4436fc42a502
taif <toofy.556@hotmail.com>
2022-12-23 01:42:54 -0800

    taif - profile styling update

--- Deleviry App/Deleviry App/profile/profileView.swift
+++ Deleviry App/Deleviry App/profile/profileView.swift
@@ -13,24 +13,39 @@ struct profileView: View {
     @State private var showPhotoPickerSheet = false
     var body: some View {
         NavigationView{
+           
+            
             VStack{
-                ZStack{
-                    Image(uiImage: profilImage!).prifileImageStyle()
-                        .padding()
-                        .onTapGesture {
-                            showPhotoPickerSheet = true
-                            print("hi")
-                    }
-                }
+                Text("Welcome!")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color(red: 1.031, green: 0.837, blue: 0.772))
+                    
                 ProfileSettings()
-                    .offset(y: 50)
+                    .offset(y: 145)
                     .ignoresSafeArea(edges: .bottom)
                     .padding(.bottom)
+               
+                Image(uiImage: profilImage!).prifileImageStyle()
+                    .padding(.bottom)
+                    .offset(y: -500)
+                    .onTapGesture {
+                        showPhotoPickerSheet = true
+                        print("hi")
+                    }
             }.padding()
              .sheet(isPresented: $showPhotoPickerSheet) {
                  ImagePicker(sourceType: .photoLibrary, selectedImage: self.$profilImage)
              }
-        }.navigationTitle("Welcom!")
+            ZStack {
+                Image("plate&fork")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .opacity(0.1)
+                           }
+        }
     }
         
     struct SwiftUIView_Previews: PreviewProvider {
